e06c669e94db2ca169743084f548af94518adc03
richfjiq <rfjiq1986@gmail.com>
2023-07-30 18:21:09 -0600

    chore: add styles to navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,47 @@
 import { ReactComponent as Logo } from '../assets/logo.svg';
+import { ReactComponent as Menu } from '../assets/hamburger_menu.svg';
+import { ReactComponent as Basket } from '../assets/basket.svg';
 
 const Navbar = () => {
   return (
-    <nav>
-      <Logo />
-      <ul>
-        <li>
-          <a href="/home">Homepage</a>
-        </li>
-        <li>
-          <a href="/about">About Us</a>
-        </li>
-        <li>
-          <a href="/blog">Blog</a>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar_container">
+        <Menu className="navbar_menu_icon" />
+        <Logo className="navbar_logo" />
+        <Basket className="navbar_basket_icon" />
+        <ul className="navbar_links_container">
+          <li>
+            <a className="navbar_link" href="#home">
+              Homepage
+            </a>
+          </li>
+          <li>
+            <a className="navbar_link" href="#about">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a className="navbar_link" href="#menu">
+              Menu
+            </a>
+          </li>
+          <li>
+            <a className="navbar_link" href="#reservations">
+              Reservations
+            </a>
+          </li>
+          <li>
+            <a className="navbar_link" href="#order_online">
+              Order Online
+            </a>
+          </li>
+          <li>
+            <a className="navbar_link" href="#login">
+              Login
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
